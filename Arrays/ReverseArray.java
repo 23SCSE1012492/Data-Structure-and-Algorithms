3479e91e7148b4ee2334b0162f9c10a573397a19
Md Tausif <razatausif2510@gmail.com>
2025-11-30 22:22:45 +0530

    Add files via upload

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,23 @@
+public class ReverseArray {
+    public static void reverse(int num[]) {
+        int start=0; int last=num.length-1;
+        while(start<last){
+            int temp=num[last];
+            num[last]=num[start];
+            num[start]=temp;
+
+            start++;
+            last--;
+        }
+    }
+    public static void main(String[] args) {
+        int num[]={1,2,3,4,5};
+        reverse(num);
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+        }
+        System.out.println();
+    }
+}
+
+
